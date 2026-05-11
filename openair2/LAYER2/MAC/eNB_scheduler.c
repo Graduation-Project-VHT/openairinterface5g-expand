43714beb9cf5012dda2b2a63daa82cae41049536
Felix <phandinhquy1103@gmail.com>
2026-05-11 10:44:20 +0700

    Add switch to another scheduler

--- openair2/LAYER2/MAC/eNB_scheduler.c
+++ openair2/LAYER2/MAC/eNB_scheduler.c
@@ -948,6 +948,10 @@ eNB_dlsch_ulsch_scheduler(module_id_t module_idP,
   void (*schedule_ulsch_p)(module_id_t module_idP, frame_t frameP, sub_frame_t subframe) = NULL;
   void (*schedule_ue_spec_p)(module_id_t module_idP, frame_t frameP, sub_frame_t subframe, int *mbsfn_flag) = NULL;
 
+  #ifndef SCHED_MODE_MLWDF
+  #define SCHED_MODE_MLWDF 2
+  #endif
+
   if (eNB->scheduler_mode == SCHED_MODE_DEFAULT) {
     schedule_ulsch_p = schedule_ulsch;
     schedule_ue_spec_p = schedule_dlsch;
@@ -955,6 +959,9 @@ eNB_dlsch_ulsch_scheduler(module_id_t module_idP,
     memset(dlsch_ue_select, 0, sizeof(dlsch_ue_select));
     schedule_ulsch_p = schedule_ulsch_fairRR;
     schedule_ue_spec_p = schedule_ue_spec_fairRR;
+  } else if (eNB->scheduler_mode == SCHED_MODE_MLWDF) {
+    schedule_ulsch_p = schedule_ulsch;
+    schedule_ue_spec_p = schedule_dlsch;
   }
 
   if(debug_flag == 0) {
